cc9b2853bc7a58ae5544691e40f4167b4c8ebc7e
OwenRC0028229 <RC0028229@CRO-24-14>
2018-09-18 10:42:13 +0100

    Moving in a square

--- Follow Path/Form1.cs
+++ Follow Path/Form1.cs
@@ -16,5 +16,29 @@ namespace Follow_Path
         {
             InitializeComponent();
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                await Task.Delay(100);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+                await Task.Delay(100);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+                await Task.Delay(100);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
+                await Task.Delay(100);
+            }
+        }
     }
 }
